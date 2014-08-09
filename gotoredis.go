7b170dca669c6703b76d036da2b56f99d8813668
Craig Furman <craig.furman89@gmail.com>
2014-08-09 08:33:52 +0100

    interface to saving

--- gotoredis.go
+++ gotoredis.go
@@ -1,1 +1,11 @@
 package gotoredis
+
+type StructMapper struct{}
+
+func New(redisEndpoint string) *StructMapper {
+	return &StructMapper{}
+}
+
+func (StructMapper) Save(obj interface{}) error {
+	return nil
+}
--- gotoredis_test.go
+++ gotoredis_test.go
@@ -1,9 +1,41 @@
 package gotoredis_test
 
 import (
+	"github.com/craigfurman/gotoredis"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("saving structs in Redis", func() {
+type SimpleStruct struct {
+	Name string
+}
+
+var _ = Describe("saving objects in Redis", func() {
+
+	var g *gotoredis.StructMapper
+
+	Context("when Redis is running on expected host and port", func() {
+
+		BeforeEach(func() {
+			g = gotoredis.New("localhost:4567")
+		})
+
+		Context("when a struct is saved", func() {
+
+			var err error
+
+			BeforeEach(func() {
+				value := "some string"
+				toBeSaved := SimpleStruct{
+					Name: value,
+				}
+				err = g.Save(toBeSaved)
+			})
+
+			It("does not error", func() {
+				Expect(err).ToNot(HaveOccurred())
+			})
+		})
+	})
 })
